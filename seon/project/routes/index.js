c25348adbfc47705917a04e0461eba2f783a0462
jongwhi <31754161+jongwhi@users.noreply.github.com>
2017-11-15 05:05:53 +0900

    db1

--- seon/project/routes/index.js
+++ seon/project/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,4 +32,20 @@ router.get('/review', function(req, res, next) {
   res.render('review', { title: 'Express' });
 });
 
+router.post('/insert', function(req,res,next){
+var newUser = new User();
+newUser.username = req.body.username;
+newUser.password = req.body.password;
+newUser.nickname = req.body.nickname;
+newUser.gender = req.body.gender;
+
+newUser.save(function(err){
+if(err){
+console.error(err);
+res.json({result: 0});
+return;
+}
+res.redirect('/');
+});
+});
 module.exports = router;
