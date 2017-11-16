58b96d78c169e27317424d1f0043c6fc40046c3e
jongwhi <31754161+jongwhi@users.noreply.github.com>
2017-11-17 08:54:54 +0900

    login1

--- seon/project/routes/index.js
+++ seon/project/routes/index.js
@@ -1,35 +1,49 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user')
+var mongoose = require('mongoose');
+var ejs = require('ejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title : 'Fooriend' });
 });
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+    res.render('login', { title: 'Fooriend' });
+});
+
+
+router.post('/gologin', function(req, res, next) {
+   User.findOne({username: req.body.username, password: req.body.password}, function(err, users){
+       if(err){
+           console.error(err);
+           res.json({result:0});
+           return;
+       }
+       res.json(users);
+   });
 });
 
 /* GET signup page. */
 router.get('/regi', function(req, res, next) {
-  res.render('regi', { title: 'Express' });
+  res.render('regi', { title: 'Fooriend' });
 });
 router.get('/board', function(req, res, next) {
-  res.render('board', { title: 'Express' });
+  res.render('board', { title: 'Fooriend' });
 });
 router.get('/store', function(req, res, next) {
-  res.render('store', { title: 'Express' });
+  res.render('store', { title: 'Fooriend' });
 });
 router.get('/lank', function(req, res, next) {
-  res.render('lank', { title: 'Express' });
+  res.render('lank', { title: 'Fooriend' });
 });
 router.get('/search', function(req, res, next) {
-  res.render('search', { title: 'Express' });
+  res.render('search', { title: 'Fooriend' });
 });
 router.get('/review', function(req, res, next) {
-  res.render('review', { title: 'Express' });
+  res.render('review', { title: 'Fooriend' });
 });
 
 router.post('/insert', function(req,res,next){
