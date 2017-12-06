130038710a7da214808684940c47053be9e3b7ce
jongwhi <31754161+jongwhi@users.noreply.github.com>
2017-12-07 08:41:24 +0900

    ?

--- seon/project/models/user.js
+++ seon/project/models/user.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // 몽구스 요청하고 필드 정의
 var userSchema = new Schema({
-    username: {type:String, required:true, unique:true},
-    password: {type:String, required:true},
-    nickname: {type:String, required:true, unique:true},
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    nickname: {type: String, required: true, unique: true},
     gender: String,
-    createdAt: {type:Date, default:Date.now},
+    createdAt: {type: Date, default: Date.now}
 });
 
 // 실제로 사용자 모델만들고 내보내기
